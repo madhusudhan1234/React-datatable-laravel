b51601085ef5b48b4fcaa298f239a213cfb91dfe
sadhakbj <sadhakbj@gmail.com>
2018-11-20 11:34:34 +0545

    Initial packages setup

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /**
+     * @param Request $request
+     */
+    public function getUsersForDataTable(Request $request)
+    {
+//      $users =
+    }
+}
